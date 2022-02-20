b362941a89ada389e7a485bba5f57331ef6dd620
Louis <6723574+louisgv@users.noreply.github.com>
2022-02-19 23:08:58 -0400

    v1.0.0 - basic API implemented

--- index.ts
+++ index.ts
@@ -1,5 +1,41 @@
-#!/usr/bin/env node
+import { signAddon } from "sign-addon"
 
-export const life = 42
+export type Options = {
+  apiKey: string
+  apiSecret: string
 
-console.log("HELLO WORLD", life)
+  extId?: string
+}
+
+const requiredFields = ["apiKey", "apiSecret"]
+
+export class MozillaWebstoreClient {
+  options = {} as Options
+
+  constructor(options: Options) {
+    for (const field of requiredFields) {
+      if (!options[field]) {
+        throw new Error(`Option "${field}" is required`)
+      }
+
+      this.options[field] = options[field]
+    }
+
+    if (typeof options.extId === "string" && options.extId.length > 0) {
+      this.options.extId = options.extId
+    }
+  }
+
+  async submit({ filePath, version = "1.0.0" }) {
+    return signAddon({
+      // Required arguments:
+
+      xpiPath: filePath,
+      version,
+      apiKey: this.options.apiKey,
+      apiSecret: this.options.apiSecret,
+
+      id: this.options.extId
+    })
+  }
+}
