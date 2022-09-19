22a1fc11519fd2e69a5dd3c5067c6e34004f517c
Louis <6723574+louisgv@users.noreply.github.com>
2022-09-19 00:29:57 -0400

    chore: bump dependencies

--- jest.config.mjs
+++ jest.config.mjs
@@ -7,11 +7,10 @@ const config = {
 
   clearMocks: true,
   moduleFileExtensions: ["js", "ts"],
-  preset: "ts-jest/presets/default-esm",
-  globals: {
-    "ts-jest": {
+  transform: {
+    "^.+\\.ts$": ["ts-jest", {
       useESM: true
-    }
+    }]
   },
   testMatch: ["**/*.test.ts"],
   verbose: true,
